a216b30d09645f901e24bc88084d6797c5fce93d
Petruz <petrairisl36@gmail.com>
2017-10-01 20:29:26 -0700

    Tried to change between views, it didn't work however

--- CardStack/app/src/main/java/com/example/petruz/cardstack/LinearLayout.java
+++ CardStack/app/src/main/java/com/example/petruz/cardstack/LinearLayout.java
@@ -2,6 +2,7 @@ package com.example.petruz.cardstack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LinearLayout extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class LinearLayout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linear_layout);
+    }
+
+    public void changeLayout(View view)
+    {
+       // setContentView(R.layout.relativelayout);
+
     }
 }
--- CardStack/app/src/main/java/com/example/petruz/cardstack/RelativeLayout.java
+++ CardStack/app/src/main/java/com/example/petruz/cardstack/RelativeLayout.java
@@ -1,8 +1,18 @@
 package com.example.petruz.cardstack;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by Petruz on 01/10/17.
  */
 
 public class RelativeLayout {
+
+    public RelativeLayout()
+    {
+
+    }
 }
