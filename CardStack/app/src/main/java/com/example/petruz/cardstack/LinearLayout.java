211dc578c69889fe78e2688642e69560d49974a5
Petruz <petrairisl36@gmail.com>
2017-10-02 21:16:24 -0700

    added all 52 cards

--- CardStack/app/src/main/java/com/example/petruz/cardstack/LinearLayout.java
+++ CardStack/app/src/main/java/com/example/petruz/cardstack/LinearLayout.java
@@ -17,40 +17,37 @@ public class LinearLayout extends AppCompatActivity {
 
     private static final String TAG = "MyActivity";
 
-    private static String H = "heart";
-    private static String C = "clove";
-    private static String D = "diamonds";
-    private static String S = "space";
-
-    protected ImageView imageView;
-
     public Stack<Card> cardStack = new Stack<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linear_layout);
-        Log.d(TAG, "on create");
 
-       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //1-14 in the loops to get nr 1-13 instead of 0-12
+        for(int i =1; i<14; i++)
+        {
+            Card c = new Card(i, Card.Suit.HEART);
+            cardStack.push(c);
+        }
 
+        for(int i =1; i<14; i++)
+        {
+            Card c = new Card(i, Card.Suit.SPACE);
+            cardStack.push(c);
+        }
 
-        Card c = new Card(5, Card.Suit.HEART);
-        Card c2 = new Card(6, Card.Suit.SPACE);
-        Card c3 = new Card(1, Card.Suit.CLOVE);
-        Card c4 = new Card(11, Card.Suit.DIAMONDS);
-        Card c5 = new Card(12, Card.Suit.DIAMONDS);
-        Card c6 = new Card(13, Card.Suit.SPACE);
-        Card c7 = new Card (2, Card.Suit.HEART);
+        for(int i =1; i<14; i++)
+        {
+            Card c = new Card(i, Card.Suit.DIAMONDS);
+            cardStack.push(c);
+        }
 
-
-        cardStack.push(c3);
-        cardStack.push(c4);
-        cardStack.push(c5);
-        cardStack.push(c6);
-        cardStack.push(c);
-        cardStack.push(c7);
-        cardStack.push(c2);
+        for(int i =1; i<14; i++)
+        {
+            Card c = new Card(i, Card.Suit.CLOVE);
+            cardStack.push(c);
+        }
 
         Collections.shuffle(cardStack);
 
@@ -90,7 +87,6 @@ public class LinearLayout extends AppCompatActivity {
         TextView numTv2 = (TextView) findViewById(R.id.num2);
         ImageView smallImg2 = (ImageView) findViewById(R.id.smallSuit2);
 
-        Log.d(TAG, "change card");
         numTv.setText(topCard.cardNr);
         numTv.setTextColor(topCard.cardColor);
         smallImg.setImageResource(topCard.suit);
@@ -98,13 +94,10 @@ public class LinearLayout extends AppCompatActivity {
         numTv2.setText(topCard.cardNr);
         numTv2.setTextColor(topCard.cardColor);
         smallImg2.setImageResource(topCard.suit);
-
-
-
-
+        
         for(int i = 0; i<topCard.nr; i++)
         {
-            imageView = new ImageView(this);
+            ImageView imageView = new ImageView(this);
             imageView.setImageResource(topCard.suit);
             layout.addView(imageView);
         }
